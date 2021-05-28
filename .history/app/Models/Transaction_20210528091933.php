c46b77c8dbc0b4b9adbbf924cdb7a6fe1189353f
Louie <vincent.abad@reddragonfarm.com>
2021-05-28 10:26:32 +0800

    Major Changes on Transaction Databases and Search of Category Controller

---
+++ .history/app/Models/Transaction_20210528091933.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    use HasFactory;
+
+    protected $table = 'transactions';
+
+    protected $fillable = [
+        'users_id'
+        ,'id_prefix'
+        ,'id_prefix'
+        ,'id_prefix'
+        ,'id_prefix'
+        ,'id_prefix'
+        ,'id_prefix'
+        ,'id_prefix'
+        ,'id_prefix'
+        ,'id_prefix'
+        ,'id_prefix'
+        ,'id_prefix'
+        ,'id_prefix'
+        ,'id_prefix'
+        ,'id_prefix'
+        ,'id_prefix'
+        ,'id_prefix'
+        ,'id_prefix'
+        ,'id_prefix'
+        ,'id_prefix'
+        ,'id_prefix'
+        ,'id_prefix'
+        ,'id_prefix'
+        ,'id_prefix'
+        ,'id_prefix'
+        ,'id_prefix'
+        ,'id_prefix'
+
+    ];
+}
---
+++ .history/database/migrations/2021_05_27_092651_create_referrence_batches_table_20210527172936.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReferrenceBatchesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('referrence_batches', function (Blueprint $table) {
+            $table->id();
+            $table->string('referrence_type');
+            $table->bigInteger('rr_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('referrence_batches');
+    }
+}
