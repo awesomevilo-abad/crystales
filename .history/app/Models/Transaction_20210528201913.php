59b76794bdc2824968ccb5d97bf0c549ad9c59ca
Louie <vincent.abad@reddragonfarm.com>
2021-06-29 08:29:30 +0800

    Update New Controllers

---
+++ .history/app/Models/Transaction_20210528201913.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    use HasFactory;
+
+    protected $table = 'transactions';
+
+    protected $fillable = [
+        'users_id'
+        , 'id_prefix'
+        , 'id_no'
+        , 'first_name'
+        , 'middle_name'
+        , 'last_name'
+        , 'suffix'
+        , 'department'
+        , 'transaction_id'
+        , 'tag_id'
+        , 'document_id'
+        , 'document_type'
+        , 'document_date'
+        , 'category_id'
+        , 'category'
+        , 'company_id'
+        , 'company'
+        , 'supplier_id'
+        , 'supplier'
+        , 'po_group'
+        , 'referrence_group'
+        , 'date_requested'
+        , 'remarks'
+        , 'payment_type'
+        , 'status'
+        , 'reason_id'
+        , 'reason'
+        , 'document_no'
+        , 'document_amount',
+
+    ];
+
+    protected $cast = [
+        'po_group'=>'array',
+        'po_group'=>'array',
+    ]
+
+}
